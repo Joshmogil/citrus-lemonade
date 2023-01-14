50432f5720ecedc789b596761252b62b824177f3
Josh Mogil <jmogil@pop-os.localdomain>
2023-01-14 11:36:42 -0500

    homework

--- euclid.c
+++ euclid.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
-int gcd(int u, int v)
-    {
-        int t;
-        while (u > 0)
-            {
-                if (u < v)
-                    {t = u; u = v; v = t;}
-                u = u-v;
-            }
-        return v;
+struct Fraction { int numerator; int denominator; };
+
+struct Fraction gcd(int u, int v) {
+    struct Fraction fr;
+    fr.numerator=u;
+    fr.denominator=v;
+    // trying to simplify a fraction here
+
+    while (fr.denominator > 0)
+        {
+            fr.denominator = fr.denominator-fr.numerator;
+        }
+
+    printf("%d",fr.denominator);
+    return fr;
     }
 main()
     {
         int x, y;
+        struct Fraction j;
         while (scanf("%d %d", &x, &y) != EOF)
             if (x>0 && y>0)
-                printf("%d %d %d\n", x, y, gcd(x,y));
+
+                j = gcd(x,y);
+                int n = j.numerator;
+                int d = j.denominator;
+
+                printf("%d %d %d/%d\n", x, y, n, d);
     }
