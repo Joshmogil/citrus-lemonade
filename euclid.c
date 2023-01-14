9f1c31f6a60af9de129fd5a642d723f93a20f5da
Josh Mogil <jmogil@pop-os.localdomain>
2023-01-14 18:39:34 -0500

    cccc-ucsess!

--- euclid.c
+++ euclid.c
@@ -2,30 +2,44 @@
 
 struct Fraction { int numerator; int denominator; };
 
-struct Fraction gcd(int u, int v) {
-    struct Fraction fr;
-    fr.numerator=u;
-    fr.denominator=v;
-    // trying to simplify a fraction here
 
-    while (fr.denominator > 0)
-        {
-            fr.denominator = fr.denominator-fr.numerator;
-        }
 
-    printf("%d",fr.denominator);
-    return fr;
+
+
+int gcd(int u, int v) {
+    int t;
+    while (u > 0)
+    {
+        if (u < v)
+            { t = u; u = v; v = t;}
+        u = u - v;
     }
+    return v;
+    }
+
+struct Fraction simplify(struct Fraction fr){
+    int comdom = gcd(fr.numerator,fr.denominator);
+    fr.numerator = fr.numerator/comdom;
+    fr.denominator = fr.denominator/comdom;
+    return fr;
+}
+
 main()
     {
         int x, y;
         struct Fraction j;
         while (scanf("%d %d", &x, &y) != EOF)
-            if (x>0 && y>0)
+            if (x>0 && y>0){
+                j.denominator = y;
+                j.numerator = x;
 
-                j = gcd(x,y);
+                j = simplify(j);
+
                 int n = j.numerator;
                 int d = j.denominator;
 
-                printf("%d %d %d/%d\n", x, y, n, d);
+                printf("%d/%d -> %d/%d\n", x, y, n, d);
+            }
+
+                
     }
