8331f4185bfa05d35ffde21de7bd15acaf4cc8fa
Josh Mogil <jmogil@pop-os.localdomain>
2023-01-16 16:28:40 -0500

    decimal to binary in go

--- chapter1/binarize.go
+++ chapter1/binarize.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func decimalToBinary(numer int) int {
+func arrayToString(a []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a)," ", delim, -1), "[]")
+} 
+
+func decimalToBinary(numer int) string {
 	var rStore []int
 
 	for (numer>0) {
-		rStore = append(rStore, (numer % 2))
+		remain := numer % 2
+		rStore = append(rStore, remain)
 		numer = numer / 2
+		fmt.Printf("%d , remainder %d\n", numer, remain)
 	}
-	return rStore[0]
+
+	for i, j := 0, len(rStore) -1; i < j; i, j = i+1, j-1 {
+		rStore[i], rStore[j] = rStore[j], rStore[i]
+	}
+
+	return arrayToString(rStore,"")
 }
 
 func main(){
 	var input int
 	fmt.Scanln(&input)
 	th := decimalToBinary(input)
-	fmt.Printf("%d",th)
+	fmt.Printf("%s",th)
 }
